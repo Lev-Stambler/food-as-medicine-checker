862e1253802d63a7edd9c14d275c3a1ef7e387c5
Lev Stambler <levstamb@gmail.com>
2020-06-08 13:28:58 -0400

    Add Elaine Moran scraper

--- packages/health-site-scraper/lib/parsers/elaine-moran-wellness-parser.ts
+++ packages/health-site-scraper/lib/parsers/elaine-moran-wellness-parser.ts
@@ -7,7 +7,7 @@ import * as cheerio from "cheerio";
 export const ElainemoranWellnessParser: IParser = {
   parserF: async (html) => {
     const $ = cheerio.load(html);
-    const symptom = $("h1.entry-title").text();
+    const symptom = $("h1.entry-title").text().toLowerCase();
     // remove parantheses which are used as side notes
     const foodItems = $("div.thrv_wrapper ul li")
       .map((i, el) =>
@@ -15,6 +15,7 @@ export const ElainemoranWellnessParser: IParser = {
           .text()
           .replace(/\(.*\)/g, "")
           .replace("&", "and")
+          .toLowerCase()
       )
       .get();
     let res: IHealthRemedies[] = [
--- packages/health-site-scraper/lib/scraper/health-site-scraper.ts
+++ packages/health-site-scraper/lib/scraper/health-site-scraper.ts
@@ -12,21 +12,46 @@ async function getSiteSourceHTML(url: string): Promise<string> {
 }
 
 /**
- * Finds associated health remedies for a particular website
+ * Finds associated health remedies found on one page on a particular website
+ * @param parser
  * @param url
- * @param parser the necessary parser for a specific URL
  */
-export async function scrapeHealthSite(
-  url: string,
-  parser: IParser
+async function scrapeHealthSiteSinglePage(
+  parser: IParser,
+  url: string
 ): Promise<IHealthRemedies[]> {
   const html = await getSiteSourceHTML(url);
   return await parser.parserF(html);
 }
 
-export async function runAllScrapers() {
-  await scrapeHealthSite(
-    "https://www.elainemoranwellness.com/food-as-medicine-database/search-by-health-condition/acne-clearing/",
-    parsers.ElainemoranWellnessParser
+/**
+ * Finds associated health remedies found on a particular website
+ * @param urls
+ * @param parser the necessary parser for a specific URL
+ */
+export async function scrapeHealthSite(
+  parser: IParser,
+  ...urls: string[]
+): Promise<IHealthRemedies[]> {
+  // create an array of promises to concurrently perform web scraping
+  const pageScrapingProms = urls.map((url) =>
+    scrapeHealthSiteSinglePage(parser, url)
   );
+  const healthRemedies = await Promise.all(pageScrapingProms);
+
+  // Because each individual page returns an array of health remedies
+  // healthRemedies will be an array of arrays which should be flattened
+  return healthRemedies.flat();
+}
+
+export async function runAllScrapers() {
+  const remedies = await scrapeHealthSite(
+    parsers.ElainemoranWellnessParser,
+    "https://www.elainemoranwellness.com/food-as-medicine-database/search-by-health-condition/muscle-building/",
+    "https://www.elainemoranwellness.com/food-as-medicine-database/search-by-health-condition/energy-increasing/",
+    "https://www.elainemoranwellness.com/food-as-medicine-database/search-by-health-condition/blood-sugar-balancing/",
+    "https://www.elainemoranwellness.com/food-as-medicine-database/search-by-health-condition/brain-improving/",
+    "https://www.elainemoranwellness.com/food-as-medicine-database/search-by-health-condition/acne-clearing/"
+  );
+  console.log(remedies);
 }
