b490eb319b0b545094df81385fe99b24c1d7c615
Lev Stambler <levstamb@gmail.com>
2020-06-10 11:24:43 -0400

    temporarly right out

--- apps/scraper-runner/src/main.ts
+++ apps/scraper-runner/src/main.ts
@@ -1,5 +1,6 @@
 import * as scholarsScrape from '@foodmedicine/scholars-scraper';
 import * as articleParser from '@foodmedicine/article-parser';
+import * as fs from 'fs';
 
 async function run() {
   const mockRemedey = {
@@ -8,17 +9,22 @@ async function run() {
   };
   const articleHeads = await scholarsScrape.runScholarsScraper(mockRemedey);
   console.log(articleHeads[0]);
-  const p = await articleParser.evaluateArticle(
-    articleHeads[0],
-    articleParser.EbiParser
-  );
-  console.log('Sorting now!');
-  console.log(
-    p.paragraphs.sort((a, b) => a.correlationScore - b.correlationScore)
-  );
-  // const downloadProms = articleHeads.map((articleHead) =>
-  //   articleParser.evaluateArticle(articleHead, articleParser.EbiParser)
+  // const p = await articleParser.evaluateArticle(
+  //   articleHeads[0],
+  //   articleParser.EbiParser
   // );
-  // await Promise.all(downloadProms);
+  // console.log('Sorting now!');
+  // console.log(
+  //   p.paragraphs.sort((a, b) => a.correlationScore - b.correlationScore)
+  // );
+  const downloadProms = articleHeads.map(async (articleHead, i) => {
+    const ret = await articleParser.evaluateArticle(
+      articleHead,
+      articleParser.EbiParser
+    );
+    fs.writeFileSync(`test-out/${i}.json`, JSON.stringify(ret))
+    return ret;
+  });
+  const res = await Promise.all(downloadProms);
 }
 run();
