3a4850f4d77567480399619608e765f31204dd13
Lev Stambler <levstamb@gmail.com>
2020-06-12 08:58:16 -0400

    add columns to table

--- apps/table-frontend/src/app/pages/table.tsx
+++ apps/table-frontend/src/app/pages/table.tsx
@@ -1,27 +1,43 @@
 import React from 'react';
 import { useTable } from 'react-table';
+import MaterialTable from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
 const data = [
   {
     impacted: 'brain',
     recommendation: 'ginger',
     effective: 'No',
     confidence: '10',
-    fromResearch: 'aaas',
+    fromResearch: ({...props}) => <div>Hello</div>,
   },
 ];
 const columns = [
   {
     Header: 'Impacted',
-    columns: [
-      {
-        Header: 'impacted',
-        accessor: 'impacted',
-      },
-      {
-        Header: 'impacted',
-        accessor: 'impacated',
-      },
-    ],
+    accessor: 'impacted',
+  },
+  {
+    Header: 'Recommendation',
+    accessor: 'recommendation',
+  },
+  {
+    Header: 'Is it effective?',
+    accessor: 'effective',
+  },
+  {
+    Header: 'How sure are we?',
+    accessor: 'confidence',
+  },
+  {
+    Header: () => <strong>Check out the research</strong>,
+    expander: true,
+    Expander: ({ isExpanded, ...rest }) => (
+      <div>{isExpanded ? <span>Hello</span> : <span>Goodbye</span>}</div>
+    ),
+    accessor: 'fromResearch',
   },
 ];
 const Table = (props) => {
@@ -37,31 +53,35 @@ const Table = (props) => {
   });
   return (
     <div className="app">
-      <table {...getTableProps()}>
-        <thead>
+      <MaterialTable {...getTableProps()}>
+        <TableHead>
           {headerGroups.map((headerGroup) => (
-            <tr {...headerGroup.getHeaderGroupProps()}>
+            <TableRow {...headerGroup.getHeaderGroupProps()}>
               {headerGroup.headers.map((column) => (
-                <th {...column.getHeaderProps()}>{column.render('Header')}</th>
+                <TableCell {...column.getHeaderProps()}>
+                  {column.render('Header')}
+                </TableCell>
               ))}
-            </tr>
+            </TableRow>
           ))}
-        </thead>
-        <tbody {...getTableBodyProps()}>
+        </TableHead>
+        <TableBody {...getTableBodyProps()}>
           {rows.map((row, i) => {
             prepareRow(row);
             return (
-              <tr {...row.getRowProps()}>
+              <TableRow {...row.getRowProps()}>
                 {row.cells.map((cell) => {
                   return (
-                    <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+                    <TableCell {...cell.getCellProps()}>
+                      {cell.render('Cell')}
+                    </TableCell>
                   );
                 })}
-              </tr>
+              </TableRow>
             );
           })}
-        </tbody>
-      </table>
+        </TableBody>
+      </MaterialTable>
     </div>
   );
 };
