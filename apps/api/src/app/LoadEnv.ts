4c400abac71c0841ab52943a2886bdb3c78aa622
Lev Stambler <levstamb@gmail.com>
2020-06-17 16:47:18 -0400

    setup env

--- apps/api/src/app/LoadEnv.ts
+++ apps/api/src/app/LoadEnv.ts
@@ -1,21 +1,18 @@
-import dotenv from 'dotenv';
-import commandLineArgs from 'command-line-args';
+import * as commandLineArgs from 'command-line-args';
 
 // Setup command line options
 const options = commandLineArgs([
-    {
-        name: 'env',
-        alias: 'e',
-        defaultValue: 'development',
-        type: String,
-    },
+  {
+    name: 'env',
+    alias: 'e',
+    defaultValue: 'development',
+    type: String,
+  },
 ]);
 
-// Set the env file
-const result2 = dotenv.config({
-    path: `./env/${options.env}.env`,
-});
-
-if (result2.error) {
-    throw result2.error;
+const envVariables = require(`../environments/${options.env}.env.json`);
+for (const key in envVariables) {
+  process.env[key] = envVariables[key];
 }
+
+export default {};
--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,13 +1,10 @@
-
-import './LoadEnv'; // Must be the first import
-import app from '@server';
-import logger from '@shared/Logger';
+import app from './app/Server';
+import logger from './app/shared/Logger';
+import './app/LoadEnv';
+console.log(process.env.NODE_ENV);
 
 // Start the server
 const port = Number(process.env.PORT || 3000);
 app.listen(port, () => {
-    logger.info('Express server started on port: ' + port);
+  logger.info('Express server started on port: ' + port);
 });
-
-
-console.log('Hello World!');
