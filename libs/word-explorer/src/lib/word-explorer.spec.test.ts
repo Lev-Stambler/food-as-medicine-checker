61c94e178d58b3d2207dc6d7c6883743cc136374
Lev Stambler <levstamb@gmail.com>
2020-06-17 15:09:51 -0400

    test

--- libs/word-explorer/src/lib/word-explorer.spec.test.ts
+++ libs/word-explorer/src/lib/word-explorer.spec.test.ts
@@ -1,7 +1,9 @@
-import { wordExplorer } from './word-explorer';
+import { getSynonyms } from './word-explorer';
 
-describe('wordExplorer', () => {
-  it('should work', () => {
-    expect(wordExplorer()).toEqual('word-explorer');
+describe('getSyonyms', () => {
+  it('find an array of synonyms to each word in a string', async () => {
+    const synonyms = await getSynonyms('hello');
+    expect(synonyms.length > 1).toBeTruthy;
+    expect(synonyms.includes('hi')).toBeTruthy;
   });
 });
