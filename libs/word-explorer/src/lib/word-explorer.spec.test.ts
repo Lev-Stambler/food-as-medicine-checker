0e80739e2564bd70c8812b17759033c9ab415a16
Lev Stambler <levstamb@gmail.com>
2020-06-17 15:01:47 -0400

    add word explorer lib

---
+++ libs/word-explorer/src/lib/word-explorer.spec.test.ts
@@ -0,0 +1,7 @@
+import { wordExplorer } from './word-explorer';
+
+describe('wordExplorer', () => {
+  it('should work', () => {
+    expect(wordExplorer()).toEqual('word-explorer');
+  });
+});
---
+++ libs/word-explorer/src/lib/word-explorer.ts
@@ -0,0 +1,3 @@
+export function wordExplorer(): string {
+  return 'word-explorer';
+}
