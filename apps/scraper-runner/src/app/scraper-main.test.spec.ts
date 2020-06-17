f92e4acd72985c33f4c32d0f67e0531b8724a66b
Lev Stambler <levstamb@gmail.com>
2020-06-17 15:14:33 -0400

    fix scraper test

--- apps/scraper-runner/src/app/scraper-main.test.spec.ts
+++ apps/scraper-runner/src/app/scraper-main.test.spec.ts
@@ -3,5 +3,5 @@ import * as main from './scraper-main';
 describe("Scrape Elaine Moran's site and find correlating scholarly paragraphs", () => {
   it("should return paragraphs which are correlated to the first 3 claims on Elaine Moran's site", async () => {
     await main.main({ limit: 1, numberOfArticles: 5 });
-  }, 1000000);
+  }, 10000);
 });
