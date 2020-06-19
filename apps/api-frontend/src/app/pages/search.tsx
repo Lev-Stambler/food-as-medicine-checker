112a05978107767fb195eb4da41c46537ff8091d
Lev Stambler <levstamb@gmail.com>
2020-06-19 12:32:13 -0400

    trim filter null result and search

--- apps/api-frontend/src/app/pages/search.tsx
+++ apps/api-frontend/src/app/pages/search.tsx
@@ -1,11 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { SearchBar } from '@foodmedicine/components';
 import { environment } from '../../environments/environment';
+import { ParsedArticleParagraphStandalone } from '@foodmedicine/interfaces';
+import './search.css';
+
+function SingleResult(props: { paragraph: ParsedArticleParagraphStandalone }) {
+  return (
+    <div
+      className="single-result-container"
+      onClick={() => window.open(props.paragraph.head.xmlFullTextDownloadLink)}
+    >
+      <h4>{props.paragraph.head.title}</h4>
+      <p>...{props.paragraph.body}...</p>
+      <hr />
+    </div>
+  );
+}
 
 export default function SearchPage() {
-  async function onSearch(query: string): Promise<string> {
-    const ret = await fetch(environment.baseApiUrl + `/search?q=${query}`)
-    return ""
+  const [searchResults, setResults] = useState<
+    ParsedArticleParagraphStandalone[]
+  >([]);
+  async function onSearch(query: string): Promise<void> {
+    const ret = await fetch(environment.baseApiUrl + `/search?q=${query}`);
+    const body = await ret.json();
+    setResults(body as ParsedArticleParagraphStandalone[]);
   }
-  return <SearchBar onSearch={onSearch} />;
+  return (
+    <>
+      {' '}
+      <SearchBar<void> onSearch={onSearch} />
+      <div className="results-container">
+        {searchResults.map((result) => (
+          <SingleResult paragraph={result} />
+        ))}
+      </div>
+    </>
+  );
 }
--- libs/components/src/lib/search-bar/search-bar.tsx
+++ libs/components/src/lib/search-bar/search-bar.tsx
@@ -2,32 +2,16 @@ import React, { useState } from 'react';
 import ReactSearchBox from 'react-search-box';
 import './search-bar.css';
 
-interface SearchBarProps {
-  onSearch: (val: string) => Promise<string>;
+interface SearchBarProps<T> {
+  onSearch: (val: string) => Promise<T>;
 }
 
-export function SearchBar(props: SearchBarProps) {
+export function SearchBar<T>(props: SearchBarProps<T>) {
   const [searchVal, setSearchVal] = useState('');
-  const data = [
+  const recommendedResultsData = [
     {
-      key: 'john',
-      value: 'John Doe',
-    },
-    {
-      key: 'jane',
-      value: 'Jane Doe',
-    },
-    {
-      key: 'mary',
-      value: 'Mary Phillips',
-    },
-    {
-      key: 'robert',
-      value: 'Robert',
-    },
-    {
-      key: 'karius',
-      value: 'Karius',
+      key: 'ginger',
+      value: 'ginger for nausea',
     },
   ];
 
@@ -36,7 +20,7 @@ export function SearchBar(props: SearchBarProps) {
       <ReactSearchBox
         placeholder="Your Search"
         value={searchVal}
-        data={data}
+        data={recommendedResultsData}
         onChange={(val) => setSearchVal(val)}
         callback={(record) => console.log(record)}
       />
